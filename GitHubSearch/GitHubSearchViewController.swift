179f0da949b0f9a47a058434ccfb3092688935d3
도미닉 <kiljh@me.com>
2020-05-17 14:44:51 +0900

    초기 UI 구성

---
+++ GitHubSearch/GitHubSearchViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GitHubSearch
+//
+//  Created by joon-ho kil on 2020/05/17.
+//  Copyright © 2020 joon-ho kil. All rights reserved.
+//
+
+import UIKit
+
+class GitHubSearchViewController: UIViewController {
+    @IBOutlet var searchBar: UISearchBar!
+    @IBOutlet var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
