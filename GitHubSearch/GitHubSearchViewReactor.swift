fc57860347239a4b0cf72a7deac17523d4c5e956
도미닉 <kiljh@me.com>
2020-05-17 14:53:18 +0900

    pod install

---
+++ GitHubSearch/GitHubSearchViewReactor.swift
@@ -0,0 +1,22 @@
+//
+//  GitHubSearchViewReactor.swift
+//  GitHubSearch
+//
+//  Created by joon-ho kil on 2020/05/17.
+//  Copyright © 2020 joon-ho kil. All rights reserved.
+//
+
+import ReactorKit
+
+final class GitHubSearchViewReactor: Reactor {
+  enum Action {
+  }
+
+  enum Mutation {
+  }
+
+  struct State {
+  }
+
+  let initialState = State()
+}
