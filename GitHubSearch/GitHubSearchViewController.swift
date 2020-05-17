660c33dd8c76010e13ab3e1801150aec839519c4
도미닉 <kiljh@me.com>
2020-05-17 15:11:45 +0900

    GitHubSearchViewController 가 View 를 채택하도록 구현

--- GitHubSearch/GitHubSearchViewController.swift
+++ GitHubSearch/GitHubSearchViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
+import ReactorKit
+import RxSwift
+import RxCocoa
 
-class GitHubSearchViewController: UIViewController {
+class GitHubSearchViewController: UIViewController, View {
+    var disposeBag = DisposeBag()
+    
     @IBOutlet var searchBar: UISearchBar!
     @IBOutlet var tableView: UITableView!
     
+    func bind(reactor: GitHubSearchViewReactor) {
+      // Action
+      searchBar.rx.text
+        .map { Reactor.Action.updateQuery($0) }
+        .bind(to: reactor.action)
+        .disposed(by: disposeBag)
+
+      // State
+      reactor.state.map { $0.repos }
+        .bind(to: tableView.rx.items(cellIdentifier: "cell")) { indexPath, repo, cell in
+          cell.textLabel?.text = repo
+        }
+        .disposed(by: disposeBag)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        reactor = GitHubSearchViewReactor() // this makes `bind()` get called
     }
 
 
